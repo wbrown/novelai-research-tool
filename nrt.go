9ba10c11b346a31b4134e09edc469bb786f28831
Wes Brown <wesbrown18@gmail.com>
2021-07-09 16:32:36 -0400

    Better path handling and showing the user what is going on.

--- nrt.go
+++ nrt.go
@@ -3,21 +3,26 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/fatih/color"
 	novelai_api "github.com/wbrown/novelai-research-tool/novelai-api"
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
 
 type ContentTest struct {
-	OutputFilename string `json:"output_filename"`
+	OutputPrefix string `json:"output_prefix"`
 	PromptFilename string `json:"prompt_filename"`
 	Iterations int `json:"iterations"`
 	Generations int `json:"generations"`
 	Parameters novelai_api.NaiGenerateParams `json:"parameters"`
 	Prompt string
+	WorkingDir string
+	OutputPath string
+	PromptPath string
 	API novelai_api.NovelAiAPI
 }
 
@@ -29,49 +34,62 @@ type IterationResult struct {
 }
 
 func (ct ContentTest) performGenerations(generations int, input string) (responses []string) {
+	genResMarker := color.New(color.FgWhite, color.BgGreen).SprintFunc()
+	newLineToken := genResMarker("\\n")+"\n"
 	context := input
 	for generation := 0; generation < generations; generation++ {
 		resp := ct.API.GenerateWithParams(context, ct.Parameters)
 		responses = append(responses, resp)
-		fmt.Printf("=> %v\n", resp)
+		fmt.Printf("%v%v\n", genResMarker("=>"),
+			strings.Replace(resp,"\n", newLineToken, -1))
 		context = context + resp
 		time.Sleep(1 * time.Second)
 	}
 	return responses
 }
 
+func handleWrite(f *os.File, s string) {
+	_, err := f.WriteString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (ct ContentTest) Perform() {
-	promptBytes, err := ioutil.ReadFile(ct.PromptFilename)
+	promptMarker := color.New(color.FgWhite, color.BgBlue).SprintFunc()
+	newLineToken := promptMarker("\\n")+"\n"
+	promptBytes, err := ioutil.ReadFile(ct.PromptPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ct.Prompt = string(promptBytes)
+	f, err := os.OpenFile(ct.OutputPath,
+		os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	handleWrite(f, "[")
 	for iteration := 0; iteration < ct.Iterations; iteration++ {
-		responses := ct.performGenerations(ct.Generations, ct.Prompt)
-		f, err := os.OpenFile(ct.OutputFilename,
-			os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-		if err != nil {
-			log.Fatal(err)
+		if iteration != 0 {
+			handleWrite(f, ",\n")
 		}
+		fmt.Printf("%v%v\n", promptMarker("<="),
+			strings.Replace(ct.Prompt, "\n", newLineToken, -1))
+		responses := ct.performGenerations(ct.Generations, ct.Prompt)
 		serialized, err := json.MarshalIndent(IterationResult{
 			ct.Parameters,
 			ct.Prompt,
 			strings.Join(responses, ""),
 			responses,
-		}, "", "  ")
+		}, " ", "  ")
 		if err != nil {
 			log.Fatal(err)
 		}
-		_, err = f.Write(serialized)
-		if err != nil {
-			log.Fatal(err)
-		}
-		_, err = f.WriteString("\n")
-		if err != nil {
-			log.Fatal(err)
-		}
-		f.Close()
+		handleWrite(f, string(serialized))
+		f.Sync()
 	}
+	handleWrite(f, "]")
 }
 
 func LoadTestFromFile(path string) (test ContentTest) {
@@ -83,17 +101,29 @@ func LoadTestFromFile(path string) (test ContentTest) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	test.WorkingDir = filepath.Dir(path)
+	test.PromptPath = filepath.Join(test.WorkingDir, test.PromptFilename)
+	if _, err := os.Stat(test.PromptPath); os.IsNotExist(err) {
+		fmt.Printf("`%v` does not exist!\n", test.PromptPath)
+		os.Exit(1)
+	}
+	test.OutputPath = filepath.Join(test.WorkingDir,
+		strings.Replace(
+		test.OutputPrefix + "-" + time.Now().Format("2006-01-02T150405Z0700") + ".json",
+		":","", -1))
 	return test
 }
 
 func main() {
+	binName := filepath.Base(os.Args[0])
+
 	if len(os.Args) != 2 {
-		fmt.Printf("(NovelAI Research Tool) %s dir/test.json\n", os.Args[0])
+		fmt.Printf("%v: %s dir/test.json\n", binName, os.Args[0])
 		os.Exit(1)
 	}
 	inputPath := os.Args[1]
 	if _, err := os.Stat(inputPath); os.IsNotExist(err) {
-		fmt.Printf("`%v` does not exist!\n", inputPath)
+		fmt.Printf("%v: `%v` does not exist!\n", binName, inputPath)
 		os.Exit(1)
 	}
 	test := LoadTestFromFile(inputPath)
